b393740fdb7bfadc31151c523dbb1153083a8388
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-29 22:03:01 -0700

    Fixed by adding next() to main logic and console.error in catch

--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -9,12 +9,15 @@ module.exports = async (req, res, next) => {
     if (!req.headers.authorization) next('Invalid Login');
 
     const token = req.headers.authorization.split(' ').pop();
-    const validUser = await users.authenticateWithToken(token);
-
+    // console.log(token);
+    const validUser = await users.authenticateToken(token);
+    // console.log(validUser);
     req.user = validUser;
     req.token = validUser.token;
-
+    // console.log('HIT!');
+    next();
   } catch (e) {
+    console.error(e);
     res.status(403).send('Invalid Login');
   }
 };
