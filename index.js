985bc7109e1eddc63785b9a26e9cdaa69a18376b
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-29 18:16:23 -0700

    Fixed improper startup function import & invocation

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 // Start up DB Server
 const { db } = require('./src/auth/models/index.js');
+const { startup } = require('./src/server.js');
 db.sync()
   .then(() => {
 
     // Start the web server
-    require('./src/server.js').start(process.env.PORT);
+    startup(process.env.PORT || 3000);
   });
 
