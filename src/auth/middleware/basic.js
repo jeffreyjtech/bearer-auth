5bf53fd8890db5dee646e2afb71c57686d27520c
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-30 10:42:59 -0700

    Tweaked error handler to send() a more useful message

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -14,15 +14,16 @@ module.exports = async (req, res, next) => {
     //  IF BASE 64 CANNOT DECODE
     //  IT WILL ERROR ON THE REQUIRE STATEMENT FOR SOME REASON
     //  USE A TRY CATCH
+    //  Don't trust your dependencies
 
     req.user = await users.authenticateBasic(username, password);
 
-    if (!req.user) throw new Error;
+    // if (!req.user) throw new Error;
 
     next();
   } catch (e) {
     console.error(e);
-    res.status(403).send('Invalid Login');
+    res.status(403).send(`Invalid Login : ${e.message || ''}`);
   }
 };
 
