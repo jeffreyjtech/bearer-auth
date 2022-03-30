531bf7bc8845d1e57a0b077f186de8fcd0b698f6
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-29 18:15:57 -0700

    Refactored and bugfixed DATABASE_URL assignment

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -4,13 +4,26 @@ require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
 const userSchema = require('./users.js');
 
-const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL;
+let DATABASE_URL = '';
+switch (process.env.NODE_ENV) {
+case 'test':
+  DATABASE_URL = 'sqlite:memory:';
+  break;
+
+case 'development':
+  DATABASE_URL = 'postgresql://localhost:5432/bearer-auth-development';
+  break;
+
+case 'production':
+  DATABASE_URL = process.env.DATABASE_URL;
+  break;
+}
 
 const DATABASE_CONFIG = process.env.NODE_ENV === 'production' ? {
   dialectOptions: {
     ssl: true,
     rejectUnauthorized: false,
-  }
+  },
 } : {};
 
 const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
