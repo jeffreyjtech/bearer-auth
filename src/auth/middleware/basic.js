5eeabcecaf3f4806b896dc9ab973d8a46ed6a352
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-29 22:02:05 -0700

    Fixed authString decoding and wrapped in try.catch

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -1,21 +1,28 @@
 'use strict';
 
 const base64 = require('base-64');
-const { user } = require('../models/index.js');
+const { users } = require('../models/index.js');
 
 module.exports = async (req, res, next) => {
 
-  if (!req.headers.authorization) { return _authError(); }
-
-  let basic = req.headers.authorization;
-  let [username, pass] = base64.decode(basic).split(':');
-
+  // if (!req.headers.authorization) { return _authError(); }
   try {
-    req.user = await user.authenticateBasic(username, pass);
+
+    let basic = req.headers.authorization;
+    let authString = basic.split(' ')[1];
+    let [username, password] = base64.decode(authString).split(':'); 
+    //  IF BASE 64 CANNOT DECODE
+    //  IT WILL ERROR ON THE REQUIRE STATEMENT FOR SOME REASON
+    //  USE A TRY CATCH
+
+    req.user = await users.authenticateBasic(username, password);
+
+    if (!req.user) throw new Error;
+
     next();
   } catch (e) {
+    console.error(e);
     res.status(403).send('Invalid Login');
   }
-
 };
 
