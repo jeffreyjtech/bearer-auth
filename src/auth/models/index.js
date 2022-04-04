862e59d25db97e28b3775daee8385e70fe0d2c7b
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-04-03 21:35:13 -0700

    Renamed sequelize config object and fixed its props

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -5,37 +5,35 @@ const { Sequelize, DataTypes } = require('sequelize');
 const userSchema = require('./users.js');
 
 let DATABASE_URL = '';
-let DATABASE_CONFIG = {};
+let sequelizeConfig = {};
 switch (process.env.NODE_ENV) {
 case 'test':
   DATABASE_URL = 'sqlite:memory';
-  DATABASE_CONFIG = {logging: false};
+  sequelizeConfig = {logging: false};
   console.log('\n<----- SQL commands are not being logged ----->\n');
   break;
 
 case 'development':
   DATABASE_URL = 'postgresql://localhost:5432/bearer-auth-development';
-  DATABASE_CONFIG = {logging: true};
+  sequelizeConfig = {logging: true};
   console.log('\n<----- SQL commands are being logged ----->\n');
   break;
 
 case 'production':
   DATABASE_URL = process.env.DATABASE_URL;
-  DATABASE_CONFIG = {
+  sequelizeConfig = {
     dialectOptions: {
-      ssl: true,
-      rejectUnauthorized: false,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }, 
     },
   };
   console.log('\n<----- SQL commands are being logged ----->\n');
   break;
 }
 
-
-// console.log('DB CONFIG:\n ',DATABASE_CONFIG);
-// console.log('DB URL:\n ',DATABASE_URL);
-
-const sequelize = new Sequelize(DATABASE_URL, DATABASE_CONFIG);
+const sequelize = new Sequelize(DATABASE_URL, sequelizeConfig);
 
 // console.log('UserSchema create:\n ', userSchema(sequelize, DataTypes));
 
